5388e74071de012c626be409ffc9b6befdcc2f35
Bantik <corey@seologic.com>
2011-01-24 15:31:40 -0600

    Don't attach to a user unless the record accepts a user attribute.

--- app/controllers/ckeditor_controller.rb
+++ app/controllers/ckeditor_controller.rb
@@ -37,7 +37,7 @@ class CkeditorController < ApplicationController
 	  end
     
     @record.attributes = options
-    @record.user ||= current_user if respond_to?(:current_user)
+    @record.user ||= current_user if respond_to?(:current_user) && @record.respond_to?(:user)
     
     if @record.valid? && @record.save
       @text = params[:CKEditor].blank? ? @record.to_json(:only=>[:id, :type], :methods=>[:url, :content_type, :size, :filename, :format_created_at], :root => "asset") : %Q"<script type='text/javascript'>
